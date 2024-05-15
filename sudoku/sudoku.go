5f76ef71aacea6e389aa6b35f95707bc506fafbd
Eduardo Lonighi <edu_loni3@hotmail.com>
2024-05-15 13:29:15 +0100

    kinda working

--- sudoku/sudoku.go
+++ sudoku/sudoku.go
@@ -1,12 +1,20 @@
 package sudoku
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+)
 
 type Board [9][9]uint8
 
-type EmptySpaces struct {
+type Coord struct {
 	row int
 	col int
+}
+
+func NewCoord(row, col int) Coord {
+	return Coord{row: row, col: col}
 }
 
 type Sudoku struct {
@@ -39,26 +47,40 @@ func Solve(board Board) (Board, error) {
 	return board, errors.New("unsolvable")
 }
 
-func GetEmpty(board Board) []EmptySpaces {
-	emptySpaces := []EmptySpaces{}
+func getCoords(board Board, filled bool) []Coord {
+	spaces := []Coord{}
 
 	for rowIdx, row := range board {
 		for colIdx, cell := range row {
-			if cell == 0 {
-				emptySpaces = append(emptySpaces, EmptySpaces{row: rowIdx, col: colIdx})
+			if filled && cell != 0 {
+				spaces = append(spaces, NewCoord(rowIdx, colIdx))
+			}
+			if !filled && cell == 0 {
+				spaces = append(spaces, NewCoord(rowIdx, colIdx))
 			}
 		}
 	}
 
+	return spaces
+}
+
+func GetEmpty(board Board) []Coord {
+	emptySpaces := getCoords(board, false)
 	return emptySpaces
 }
 
+func GetFilled(board Board) []Coord {
+	filledSpaces := getCoords(board, true)
+	return filledSpaces
+}
+
 func Validate(board Board) bool {
+	// TODO: check for only one posible solution
 	_, err := Solve(board)
 	return err == nil
 }
 
-func solveRecurse(emtySpaceIdx int, emptySpaces *[]EmptySpaces, board *Board) bool {
+func solveRecurse(emtySpaceIdx int, emptySpaces *[]Coord, board *Board) bool {
 	if emtySpaceIdx == len((*emptySpaces)) {
 		return true
 	}
@@ -66,6 +88,7 @@ func solveRecurse(emtySpaceIdx int, emptySpaces *[]EmptySpaces, board *Board) bo
 	emptySpace := (*emptySpaces)[emtySpaceIdx]
 
 	for number := uint8(1); number <= 9; number++ {
+		// for _, number := range numbers {
 		if isValidCell(number, emptySpace.row, emptySpace.col, *(board)) {
 			(*board)[emptySpace.row][emptySpace.col] = number
 
@@ -101,3 +124,256 @@ func isValidCell(number uint8, row int, col int, board Board) bool {
 
 	return true
 }
+
+// TODO: make to work with generics
+// func scrambleSlice(slice *[]interface{}) {
+// 	for i := range *slice {
+// 		j := rand.Intn(i + 1)
+// 		(*slice)[i], (*slice)[j] = (*slice)[j], (*slice)[i]
+// 	}
+// }
+
+func SwapNumbersRandomly(board *Board) {
+	numbers := []uint8{1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+	// scramble numbers
+	// TODO: use func
+	for i := range numbers {
+		j := rand.Intn(i + 1)
+		numbers[i], numbers[j] = numbers[j], numbers[i]
+	}
+
+	for rowIdx, row := range *board {
+		for colIdx, cell := range row {
+			(*board)[rowIdx][colIdx] = numbers[cell-1]
+		}
+	}
+
+}
+
+func getNextEmpty(board Board) (int, int) {
+	for rowIdx, row := range board {
+		for colIdx, cell := range row {
+			if cell == 0 {
+				return rowIdx, colIdx
+			}
+		}
+	}
+
+	return -1, -1
+}
+
+func countSolutionsHelper(board *Board, counter *int) bool {
+
+	row, col := getNextEmpty(*board)
+
+	for value := uint8(1); value <= 9; value++ {
+		if !isValidCell(value, row, col, *board) {
+			continue
+		}
+
+		(*board)[row][col] = value
+
+		if row == 8 && col == 8 {
+			*counter++
+			break
+		}
+
+		if checkFullBoard(*board) {
+			*counter++
+			break
+		} else if countSolutionsHelper(board, counter) {
+			return true
+		}
+
+	}
+
+	(*board)[row][col] = 0
+	return false
+}
+
+func CountSolutions(board Board) int {
+	counter := 0
+
+	countSolutionsHelper(&board, &counter)
+
+	return counter
+}
+
+func removeCoords(board *Board, coord Coord) bool {
+
+	val := (*board)[coord.row][coord.col]
+	(*board)[coord.row][coord.col] = 0
+
+	solutions := CountSolutions(*board)
+
+	if solutions != 1 {
+		board[coord.row][coord.col] = val
+		return false
+	}
+
+	return true
+}
+
+func removeNumbers(board *Board, dificulty int) {
+
+	dificulty = max(0, min(6, dificulty))
+
+	filledSpaces := GetFilled(*board)
+
+	filledLen := len(filledSpaces)
+
+	clues := 30 - dificulty
+
+	// pick a random filled space to remove
+	coord := filledSpaces[rand.Intn(filledLen)]
+	if removeCoords(board, coord) {
+		filledLen--
+	} else {
+		removeNumbers(board, dificulty)
+		return
+	}
+
+	// TODO: final sudoku has 20 numbers, this needs to be adjusted somehow for dificulty
+	if filledLen <= clues {
+		return
+	}
+
+	diagonals := getDiagonals(coord, 1)
+	//scramble diagonals
+	// TODO: use fn
+	for i := range diagonals {
+		j := rand.Intn(i + 1)
+		diagonals[i], diagonals[j] = diagonals[j], diagonals[i]
+	}
+
+	amountOfDiagonalsToRemove := 4
+	// after removing 20 numbers, only remove 2 of the diagonals
+	if filledLen <= 60 {
+		amountOfDiagonalsToRemove = 2
+	}
+
+	// remove empty diagonals
+	finalDiagonals := []Coord{}
+	for _, coord := range diagonals {
+		if board[coord.row][coord.col] != 0 && len(finalDiagonals) < amountOfDiagonalsToRemove {
+			finalDiagonals = append(finalDiagonals, coord)
+		}
+	}
+
+	for _, coord := range finalDiagonals {
+		if removeCoords(board, coord) {
+			filledLen--
+			if filledLen <= clues {
+				return
+			}
+		}
+	}
+
+	removeNumbers(board, dificulty)
+}
+
+func getDiagonals(coord Coord, amount int) []Coord {
+	diagonals := []Coord{}
+
+	for i := 1; i <= amount; i++ {
+		if coord.row+i < 9 && coord.col+i < 9 {
+			diagonals = append(diagonals, NewCoord(coord.row+i, coord.col+i))
+		}
+		if coord.row+i < 9 && coord.col-i > 0 {
+			diagonals = append(diagonals, NewCoord(coord.row+i, coord.col-i))
+		}
+		if coord.row-i > 0 && coord.col+i < 9 {
+			diagonals = append(diagonals, NewCoord(coord.row-i, coord.col+i))
+		}
+		if coord.row-i > 0 && coord.col-i > 0 {
+			diagonals = append(diagonals, NewCoord(coord.row-i, coord.col-i))
+		}
+	}
+
+	return diagonals
+}
+
+func CreatePuzzle(dificulty int) Board {
+	puzzle := CreateSolvedBoard()
+
+	removeNumbers(&puzzle, dificulty)
+
+	return puzzle
+}
+
+func CreateSolvedBoard() Board {
+	board := Board{}
+
+	// fill 9 random spaces with numbers from 1 to 9
+	for i := uint8(0); i < 9; i++ {
+		row := rand.Intn(9)
+		col := rand.Intn(9)
+
+		for board[row][col] != 0 {
+			row = rand.Intn(9)
+			col = rand.Intn(9)
+		}
+
+		board[row][col] = i
+	}
+
+	// solve the board
+	solved, _ := Solve(board)
+
+	// the solver try to put smaller numbers first so boards are always low on first coords
+	// so i mantain the board structure but swap numbers
+	SwapNumbersRandomly(&solved)
+
+	return solved
+}
+
+func PrintBoard(board Board) {
+
+	fmt.Println(" -------------------------------------------------")
+	fmt.Println("   0 1 2 | 3 4 5 | 6 7 8")
+	fmt.Println("   =====================")
+	for rowIdx, row := range board {
+		if rowIdx%3 == 0 && rowIdx != 0 {
+			fmt.Println(" | ---------------------")
+		}
+		for colIdx, cell := range row {
+			if colIdx == 0 {
+				fmt.Printf("%v| ", rowIdx)
+			}
+			fmt.Printf("%v ", cell)
+			if (colIdx+1)%3 == 0 && colIdx != len(row)-1 {
+				fmt.Print("| ")
+			}
+		}
+		fmt.Print("\n")
+	}
+
+	fmt.Print("\n")
+}
+
+func checkFullBoard(board Board) bool {
+	for _, rowIdx := range board {
+		for _, cell := range rowIdx {
+			if cell == 0 {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
+/*
+binary map
+0 = 0000
+1 = 0001
+2 = 0010
+3 = 0011
+4 = 0100
+5 = 0101
+6 = 0110
+7 = 0111
+8 = 1000
+9 = 1001
+*/
--- sudoku/sudoku_test.go
+++ sudoku/sudoku_test.go
@@ -1,4 +1,3 @@
-
 package sudoku
 
 import (
@@ -43,6 +42,39 @@ func TestSudoku(t *testing.T) {
 	assert.Equal(t, expected, solved)
 	assert.Nil(t, err)
 
+	worstCase := Board{
+		{9, 8, 7, 6, 5, 4, 3, 2, 1},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		// -------------------
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		// -------------------
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+	}
+
+	worstCaseExpects := Board{
+		{9, 8, 7, 6, 5, 4, 3, 2, 1},
+		{1, 2, 3, 7, 8, 9, 4, 5, 6},
+		{4, 5, 6, 1, 2, 3, 7, 8, 9},
+		// -------------------
+		{2, 1, 4, 3, 6, 5, 8, 9, 7},
+		{3, 6, 5, 8, 9, 7, 1, 4, 2},
+		{7, 9, 8, 2, 4, 1, 5, 6, 3},
+		// -------------------
+		{5, 3, 2, 4, 1, 6, 9, 7, 8},
+		{6, 4, 1, 9, 7, 8, 2, 3, 5},
+		{8, 7, 9, 5, 3, 2, 6, 1, 4},
+	}
+
+	solvedWorstCase, err := Solve(worstCase)
+
+	assert.Equal(t, solvedWorstCase, worstCaseExpects)
+	assert.Nil(t, err)
+
 	unsolvable := Board{
 		{2, 0, 0, 9, 0, 0, 0, 0, 0},
 		{0, 0, 0, 0, 0, 0, 0, 6, 0},
@@ -61,5 +93,11 @@ func TestSudoku(t *testing.T) {
 
 	_, err = Solve(unsolvable)
 	assert.EqualError(t, err, "unsolvable")
+
+	newboard := CreatePuzzle(4)
+
+  PrintBoard(newboard)
+
+	assert.Equal(t, 1, CountSolutions(newboard))
 
 }
